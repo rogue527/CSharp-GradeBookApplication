3bb978831e39a7a023697e0284c08ef0ccb7f548
rogue527 <62187340+rogue527@users.noreply.github.com>
2020-03-14 16:31:35 -0500

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,6 +5,7 @@ namespace GradeBook.GradeBooks
         public StandardGradeBook(string name) : base(name)
         {
             Name = name;
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
